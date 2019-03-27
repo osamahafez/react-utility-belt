46dca86f3c0116c7f7940361067655f53e996e45
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-03-27 22:04:08 +0200

    Use layout and auxiliary in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Auxiliary from './hoc/Auxiliary';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <Auxiliary>
+        <Layout> Hello layout </Layout>
+      </Auxiliary>
     );
   }
 }
