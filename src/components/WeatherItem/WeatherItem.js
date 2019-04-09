e3f2c4d33a4fb9e1f3b9215807f9e590c924b8c1
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-04-09 22:42:12 +0200

    randomize inputs entered the weatherItem component

--- src/components/WeatherItem/WeatherItem.js
+++ src/components/WeatherItem/WeatherItem.js
@@ -8,16 +8,37 @@ import {
 } from 'reactstrap';
 import './WeatherItem.css';
 import CloudSunny from '../../assets/images/CloudSunny.png';
+import Sunny from '../../assets/images/Sunny.png';
+import Rainy from '../../assets/images/Rainy.png';
+import Lighting from '../../assets/images/Lighting.png';
 
 const weatherItem = (props) => {
+
+    // generating weather image depending on the random values of the degrees
+    let weatherImg = '';
+    if (props.high < 30 && props.high > 15 && props.low <= 15 && props.low >= 7) {
+        weatherImg=CloudSunny;
+    }
+    else if(props.high <= 15 && props.low <= 10 && props.low > 7) {
+        weatherImg=Rainy;
+    }
+    else if(props.high <= 15 && props.low <= 7) {
+        weatherImg=Lighting;
+    }
+    else {
+        weatherImg=Sunny;
+    }
+
     return ( 
         <div className='WeatherItemWrapper'>
-            <Card>
-                <CardImg top width='100%' src={CloudSunny} alt="Clould Sunny" />
+            <Card >
+                <CardImg top width='100%' src={weatherImg} alt="Weather Image" />
                 <CardBody>
                     <hr />
-                    <CardTitle className='WeatherItemCardTitle'>{props.day}</CardTitle>
-                    <CardText className='WeatherItemCardText'>{props.high}/{props.low}</CardText>
+                    <div style={{backgroundColor: '#fff'}}>
+                        <CardTitle className='WeatherItemCardTitle'>{props.day}</CardTitle>
+                        <CardText className='WeatherItemCardText'>{props.high}/{props.low}</CardText>
+                    </div>
                 </CardBody>
             </Card>
         </div> 
--- src/containers/WeatherContainer/WeatherContainer.js
+++ src/containers/WeatherContainer/WeatherContainer.js
@@ -5,11 +5,24 @@ import './WeatherContainer.css';
 class WeatherContainer extends Component {
 
 
+     
+
     render() {
+
+        // generating random degrees
+        const highDeg = Math.floor(Math.random()*(36-12+1)+12);
+        const lowDeg = (highDeg <= 20) ? Math.floor(Math.random()*(10-5+1)+5) : Math.floor(Math.random()*(18-11+1)+11);
+        const days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+        const randomDayNumber = Math.floor(Math.random()*(6-0+1)+0);
+        
         return (
             <div>
                 <h1 className='WeatherHeading'>Today's Weather</h1>
-                <WeatherItem day='Tuesday' high='30' low='18' />
+                <div style={{display: 'flex'}}>
+                    <WeatherItem day={days[randomDayNumber]} high={highDeg} low={lowDeg} /> 
+                    <WeatherItem day={days[(randomDayNumber+1)%7]} high={highDeg+2} low={lowDeg} />
+                    <WeatherItem day={days[(randomDayNumber+2)%7]} high={highDeg+4} low={lowDeg} />
+                </div>
             </div>
         );
     }
