457ef4f6d5370e993b4ffde98df3481e71aab6b6
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-04-11 18:03:26 +0200

    change worldClock compoent to a container

--- src/containers/WorldClock/WorldClock.js
+++ src/containers/WorldClock/WorldClock.js
@@ -1,43 +1,34 @@
-import React, {Component} from 'react';
-import {Toast, ToastHeader, ToastBody} from 'reactstrap';
+import React, { Component } from 'react';
+import { Toast, ToastHeader, ToastBody } from 'reactstrap';
 class WorldClock extends Component {
-
     state = {
         h: 0,
         m: 0,
-        s: 0,
-    }
-
+        s: 0
+    };
 
     clock = () => {
-
-        
         // get the GMT time
         let current_date = new Date();
         let seconds = current_date.getSeconds();
         let minutes = current_date.getMinutes();
-        let hours = (current_date.getHours()-2)%24; // subtract 2 because cairo is GMT +2
+        let hours = (current_date.getHours() - 2) % 24; // subtract 2 because cairo is GMT +2
 
-         // I want the s, m and h to be 2 digits at all times (e.g. 22:15:2 becomes 14:15:02)
-         if(seconds < 10)
-            seconds = "0"+seconds;
-         if(minutes < 10)
-            minutes = "0"+minutes;
-         if(hours < 10)
-            hours = "0"+hours;
+        // I want the s, m and h to be 2 digits at all times (e.g. 22:15:2 becomes 14:15:02)
+        if (seconds < 10) seconds = '0' + seconds;
+        if (minutes < 10) minutes = '0' + minutes;
+        if (hours < 10) hours = '0' + hours;
 
         this.setState({
             h: hours,
             m: minutes,
-            s: seconds,
+            s: seconds
         });
-    }
+    };
 
     minuteConverter(min) {
-        if(min < 10)
-            return min = "0"+min;
-        else 
-            return min;
+        if (min < 10) return (min = '0' + min);
+        else return min;
     }
 
     render() {
@@ -47,68 +38,93 @@ class WorldClock extends Component {
         //     this.setHours(this.getHours()+h);
         //     return this;
         // }
-        return(
+        return (
             <div className='container-fluid'>
-                <div className="p-3 my-2 rounded" style={{backgroundColor:'#2e86de'}}>
+                <div
+                    className='p-3 my-2 rounded'
+                    style={{ backgroundColor: '#2e86de' }}
+                >
                     <Toast>
                         <ToastHeader>
                             <h3>Washington</h3>
                         </ToastHeader>
                         <ToastBody className='display-3'>
-                            {(this.state.h-6)%24}:{this.state.m}:{this.state.s}
+                            {(this.state.h - 6) % 24}:{this.state.m}:
+                            {this.state.s}
                         </ToastBody>
                     </Toast>
                 </div>
-                <div className="p-3 my-2 rounded" style={{backgroundColor:'#fbc531'}}>
+                <div
+                    className='p-3 my-2 rounded'
+                    style={{ backgroundColor: '#fbc531' }}
+                >
                     <Toast>
                         <ToastHeader>
                             <h3>Cairo</h3>
                         </ToastHeader>
                         <ToastBody className='display-3'>
-                            {(this.state.h+2)%24}:{this.state.m}:{this.state.s}
+                            {(this.state.h + 2) % 24}:{this.state.m}:
+                            {this.state.s}
                         </ToastBody>
                     </Toast>
                 </div>
-                <div className="p-3 my-2 rounded" style={{backgroundColor:'#4cd137'}}>
+                {/* <div
+                    className='p-3 my-2 rounded'
+                    style={{ backgroundColor: '#4cd137' }}
+                >
                     <Toast>
                         <ToastHeader>
                             <h3>Sydney</h3>
                         </ToastHeader>
                         <ToastBody className='display-3'>
-                            {(this.state.h+10)%24}:{this.state.m}:{this.state.s}
+                            {(this.state.h + 10) % 24}:{this.state.m}:
+                            {this.state.s}
                         </ToastBody>
                     </Toast>
-                </div>
-                <div className="p-3 my-2 rounded" style={{backgroundColor:'#c0392b'}}>
+                </div> */}
+                <div
+                    className='p-3 my-2 rounded'
+                    style={{ backgroundColor: '#c0392b' }}
+                >
                     <Toast>
                         <ToastHeader>
                             <h3>London</h3>
                         </ToastHeader>
                         <ToastBody className='display-3'>
-                            {(this.state.h+1)%24}:{this.state.m}:{this.state.s}
+                            {(this.state.h + 1) % 24}:{this.state.m}:
+                            {this.state.s}
                         </ToastBody>
-                    </Toast>                    
+                    </Toast>
                 </div>
-                <div className="p-3 my-2 rounded" style={{backgroundColor:'#006266'}}>
+                {/* <div
+                    className='p-3 my-2 rounded'
+                    style={{ backgroundColor: '#006266' }}
+                >
                     <Toast>
                         <ToastHeader>
                             <h3>Rio de Janeiro</h3>
                         </ToastHeader>
                         <ToastBody className='display-3'>
-                            {(this.state.h-3)%24}:{this.state.m}:{this.state.s}
+                            {(this.state.h - 3) % 24}:{this.state.m}:
+                            {this.state.s}
                         </ToastBody>
                     </Toast>
                 </div>
-                <div className="p-3 my-2 rounded" style={{backgroundColor:'#e67e22'}}>
+                <div
+                    className='p-3 my-2 rounded'
+                    style={{ backgroundColor: '#e67e22' }}
+                >
                     <Toast>
                         <ToastHeader>
                             <h3>Mumbai</h3>
                         </ToastHeader>
                         <ToastBody className='display-3'>
-                            {(this.state.h+5)%24}:{this.minuteConverter((this.state.m+30)%60)}:{this.state.s}
+                            {(this.state.h + 5) % 24}:
+                            {this.minuteConverter((this.state.m + 30) % 60)}:
+                            {this.state.s}
                         </ToastBody>
                     </Toast>
-                </div>
+                </div> */}
             </div>
         );
     }
