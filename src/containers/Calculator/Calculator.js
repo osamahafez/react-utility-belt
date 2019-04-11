36c723f3e5ea8e46f7e2c252b4a1ad0b3968d836
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-04-11 22:54:47 +0200

    implemented getAnswer and delete methods

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -16,6 +16,11 @@ class Calculator extends Component {
         this.setState({numbersDisplay:numbersDisplay_copy});
     }
 
+    charDeleted = () => {
+        let numbersDisplay_copy = this.state.numbersDisplay.slice(0,-1);
+        this.setState({numbersDisplay:numbersDisplay_copy});
+    }
+
     clear = () => {
         this.setState({
             numbersDisplay: '', 
@@ -25,7 +30,12 @@ class Calculator extends Component {
 
     getResult = () => {
         let result = eval(this.state.numbersDisplay);
-        this.setState({result:result});
+        this.setState({result:result, numbersDisplay:''});
+    }
+
+    getAnswer = () => {
+        let numbersDisplay_copy = this.state.numbersDisplay+this.state.result;
+        this.setState({numbersDisplay:numbersDisplay_copy});
     }
 
     render() {
@@ -41,69 +51,69 @@ class Calculator extends Component {
                 </div>
                 <div className='buttons'>
                     <div className='row custom-row'>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={this.clear}><span className='cell cell-big-word'>AC</span></Button>
                         </div>
-                        <div className='col-md-3'>
-                            <Button color="info" size="lg" block><span className='cell cell-big-word'>DEL</span></Button>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
+                            <Button color="info" size="lg" block onClick={this.charDeleted}><span className='cell cell-big-word'>DEL</span></Button>
                         </div>
-                        <div className='col-md-3'>
-                            <Button color="info" size="lg" block><span className='cell cell-big-word'>ANS</span></Button>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
+                            <Button color="info" size="lg" block onClick={this.getAnswer}><span className='cell cell-big-word'>ANS</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={()=>{this.charTyped('/')}}><span className='cell'>/</span></Button>
                         </div>
                     </div>
                     <div className='row custom-row'>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('7')}}><span className='cell'>7</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('8')}}><span className='cell'>8</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('9')}}><span className='cell'>9</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={()=>{this.charTyped('*')}}><span className='cell'>*</span></Button>
                         </div>
                     </div>
                     <div className='row custom-row'>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('4')}}><span className='cell'>4</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('5')}}><span className='cell'>5</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('6')}}><span className='cell'>6</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={()=>{this.charTyped('-')}}><span className='cell'>-</span></Button>
                         </div>
                     </div>
                     <div className='row custom-row'>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('1')}}><span className='cell'>1</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('2')}}><span className='cell'>2</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('3')}}><span className='cell'>3</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={()=>{this.charTyped('+')}}><span className='cell'>+</span></Button>
                         </div>
                     </div>
                     <div className='row custom-row'>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('0')}}><span className='cell'>0</span></Button>
                         </div>
-                        <div className='col-md-3'>
+                        <div className='col-md-3 col-xs-3 col-sm-3'>
                             <Button color="info" size="lg" block onClick={() => {this.charTyped('.')}}><span className='cell'>.</span></Button>
                         </div>
-                        <div className='col-md-6'>
+                        <div className='col-md-6 col-xs-6 col-sm-6'>
                             <Button color="danger" size="lg" block onClick={this.getResult}><span className='cell'>=</span></Button>
                         </div>
                     </div>
