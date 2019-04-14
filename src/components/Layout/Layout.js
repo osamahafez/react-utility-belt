f1853e34683070f0c19369650fc2b47e825f6ce1
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-04-14 23:49:59 +0200

    added navbar to the layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './../Navbar/Navbar';
 
 const layout = (props) => {
     return (
         <div>
-            <div>NAVBAR</div>
+            <Navbar />
             <div>{props.children}</div>
         </div>
     );
