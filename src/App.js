5c29d76d3d44343b0d859bf87022d8833d27387a
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-04-15 17:15:09 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import Aux from './hoc/Auxiliary';
 import Layout from './components/Layout/Layout';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 import WeatherContainer from './containers/WeatherContainer/WeatherContainer';
 import WorldClock from './containers/WorldClock/WorldClock';
 import Calculator from './containers/Calculator/Calculator';
 
 class App extends Component {
-  
-  render() {
-      return (
-          <Aux>
-              <Layout>
-                  
-                  {/* <WeatherContainer /> */}
-                  {/* <WorldClock /> */}
-                  {/* <Calculator /> */}
-              </Layout>
-          </Aux>
-      );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Layout>
+                    <Route path='/' component={Home} exact />
+                    <Route path='/weather' component={WeatherContainer} />
+                    <Route path='/worldClock' component={WorldClock} />
+                    <Route path='/calculator' component={Calculator} />
+                </Layout>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import fannyPack from '../../assets/images/Logo/yellowFannyPack.png';
 import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-lightYellow">
-            <a className="navbar-brand" href="#"> {/* fix it using <NavLink> */}
+            <NavLink to='/' className="navbar-brand">
                 <img src={fannyPack} className="d-inline-block align-center brand-logo" alt="fanny pack" />
                 <span className='brand-name'>Fanny Pack</span>
-            </a>
+            </NavLink>
+    
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse links-custom" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="#"><span className='nav-text'>Weather</span></a>
+                        <NavLink to='/weather' className="nav-link" activeClassName="nav-activated"><span className='nav-text'>Weather</span></NavLink>                    
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#"><span className='nav-text'>World Clock</span></a>
+                        <NavLink to='/worldClock' className="nav-link" activeClassName="nav-activated"><span className='nav-text'>World Clock</span></NavLink>                    
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#"><span className='nav-text'>Calculator</span></a>
+                        <NavLink to='/calculator' className="nav-link" activeClassName="nav-activated"><span className='nav-text'>Calculator</span></NavLink>                    
                     </li>
                 </ul>
             </div>
